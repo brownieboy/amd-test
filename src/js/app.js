7ebfe74f743325b0040a0147bb31fbd04cdd5563
Michael Brown <brownieboy@gmail.com>
2015-12-31 11:40:23 +1100

    Added individual min JS files for Kendo Pro to libs folder.  Require/Define working in limited way.

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,18 @@
-console.log("app.js has been loaded");
+(function() {
+    console.log("app.js has been loaded");
+
+    require.config({
+        paths: {
+            // leave off the .js extension, otherwise it will load file.js.js and fail.
+            "jquery": "./libs/jquery-1.11.1.min"
+        }
+    });
+
+    // require(["jquery"], function($) {
+    // 	console.log("jQuery required");
+    // });
+
+    require(["./modules/testmodule"], function(testmodule) {
+        console.log("testmodule.js required");
+    });
+}())
--- src/js/modules/testmodule.js
+++ src/js/modules/testmodule.js
@@ -1,16 +1,29 @@
-define(["kendoui"], function(kendoui){
-   var Widget = kendo.ui.ObservableObject;
-   return Widget.extend({
-      init: function(element, options) {
-         Widget.fn.init.call(this,  element, options);
-         console.log("init acts as the constructor of a kendo ui component, and we can add more intialization code here...");
- 
-         //bind this presentation model to the element
-         kendo.bind($(element), this) ;
-      }
-      name: "John Doe",
-      doSomething: function(){
-         console.log("now we're returning the class itself to be instantiated elsewhere...");
-      };
-   });
+define(["jquery", "../libs/kendo/js/kendo.grid.min"], function($, kendogrid) {
+   console.log("testmodule.js loaded with kendo.grid?");
+   return {};
 });
+
+
+// define(["jquery", "../libs/kendo/js/kendo.grid"], function($, kendoui.grid){
+//    console.log("testmodule.js loaded?");
+
+// });
+
+
+
+// define(["kendoui"], function(kendoui){
+//    var Widget = kendo.ui.ObservableObject;
+//    return Widget.extend({
+//       init: function(element, options) {
+//          Widget.fn.init.call(this,  element, options);
+//          console.log("init acts as the constructor of a kendo ui component, and we can add more intialization code here...");
+ 
+//          //bind this presentation model to the element
+//          kendo.bind($(element), this) ;
+//       }
+//       name: "John Doe",
+//       doSomething: function(){
+//          console.log("now we're returning the class itself to be instantiated elsewhere...");
+//       };
+//    });
+// });
